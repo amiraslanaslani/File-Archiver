4893845af29ddd59137c8e22696ec690a156a441
Amir Aslan Aslani <aa.aslani_6@yahoo.com>
2019-03-27 14:39:15 +0430

    Debuging and NPM script added to compatibility with SQLite3

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,6 @@
-var showFileIcons = require('../JS/showFileIcons.js');
 var filesViewPage = require('../JS/filesViewPage.js');
 var fileAddPage = require('../JS/fileAddPage.js');
 
 $(() => {
     filesViewPage.load();
 })
-
-/* * * * * * * * * * * * * * * * * * * * * * * */
