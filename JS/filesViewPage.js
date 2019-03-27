7f046e6641fb306a01cbf520e5958bb292ce90be
Amir Aslan Aslani <aa.aslani_6@yahoo.com>
2019-03-28 02:46:04 +0430

    fileView's left side ended

--- JS/filesViewPage.js
+++ JS/filesViewPage.js
@@ -11,18 +11,88 @@ let rightCol = () => {
 let leftCol = () => {
     return `
         <div id="leftColTopContent">
-
+            <div id="tagSelection">
+                <input class="dark-inp" type="text" id="tagSearch" placeholder="Search">
+                <div id="tagsBox">
+                    <span>
+                        <div class="check">
+                            <input type="checkbox" data-tag="Tag Name 1">
+                            <div class="box"></div>
+                        </div>
+                        <a>
+                            Tag Name 
+                        </a>
+                    </span>
+                    <span>
+                        <div class="check">
+                            <input type="checkbox" data-tag="Tag Name 2">
+                            <div class="box"></div>
+                        </div>
+                        <a>
+                            Tag Name Is Can Be Too Long, If I Want IT!
+                        </a>
+                    </span>
+                    <span>
+                        <div class="check">
+                            <input type="checkbox" data-tag="Tag Name 3">
+                            <div class="box"></div>
+                        </div>
+                        <a>
+                            Tag Name 
+                        </a>
+                    </span>
+                    <span>
+                        <div class="check">
+                            <input type="checkbox" data-tag="Tag Name 4">
+                            <div class="box"></div>
+                        </div>
+                        <a>
+                            Tag Name 
+                        </a>
+                    </span>
+                </div>
+            </div>
         </div>
         <div id="leftColBtn">
             <span>
                 ➕ Add New File
             </span>
         </div>
+        <script>
+            filesViewPage.leftPanelLoad();
+        </script>
     `
 }
 
-exports.load = function() {
+function showRightSide(selectedTags = [], searchString = ''){
     $('#rightCol').html(rightCol());
+}
+
+function refreshView(){
+    let searchString = $('#tagSearch').val().trim();
+    let selectedTags = $('#tagsBox input:checked').map(function() {
+        return $(this).data('tag');
+    }).get();
+
+    showRightSide(selectedTags, searchString);
+}
+
+exports.leftPanelLoad = function(){
+    $('#tagsBox span').click(function(){
+        $(this).find('input[type=checkbox]').click();
+    });
+
+    $('#tagsBox span input[type=checkbox]').click(function(){
+        refreshView();
+    })
+
+    $('#tagSearch').keyup(function(){
+        refreshView();
+    })
+}
+
+exports.load = function() {
+    showRightSide();
     $('#leftCol').html(leftCol());
     $('#leftColBtn').click(() => {
         fileAddPage.load();
